aede6aed85a928c2debbacb0f60362bd465adb31
vldcher <broobroo007@gmail.com>
2017-11-22 12:13:43 +0200

    change color scheme and markup

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,15 @@
-//$(function() {
-//
-//	// Custom JS
-//
-//});
+	var hamburger = document.querySelector(".hamburger");
+	
+	// var menu = document.getElementById('menu1');
+
+	// On click
+	hamburger.addEventListener("click", function(e) {
+		hamburger.classList.toggle("is-active");
+		// menu.classList.toggle('open');
+		e.preventDefault();
+
+			// menu.classList.contains('open')?menu.classList.remove('open'):menu.classList.add('open');
+// Создать ксс класс для моб меню
+
+		
+});
