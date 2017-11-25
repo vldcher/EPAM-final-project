032eb0cbb38807a3efed75c38a14cf3c077b5ab0
vldcher <broobroo007@gmail.com>
2017-11-25 15:50:56 +0200

    finished markup for oreder part and add some images

--- app/js/common.js
+++ app/js/common.js
@@ -13,3 +13,23 @@
 
 		
 });
+
+// arrival and departure date of visit
+var today = new Date();
+var tomorrow = new Date(new Date().getTime() + 24 * 60 * 60 * 1000);
+
+// Set values
+$("#theDate").val(getFormattedDate(today));
+$("#theTomorrow").val(getFormattedDate(tomorrow));
+
+// Get date formatted as YYYY-MM-DD
+function getFormattedDate (date) {
+    return date.getFullYear()
+        + "-"
+        + ("0" + (date.getMonth() + 1)).slice(-2)
+        + "-"
+        + ("0" + date.getDate()).slice(-2);
+}
+
+document.getElementById('arrivalDate').valueAsDate = new Date();
+document.getElementById('departureDate').valueAsDate = new Date(new Date().getTime() + 24 * 60 * 60 * 1000);
--- gulpfile.js
+++ gulpfile.js
@@ -27,7 +27,7 @@ gulp.task('common-js', function() {
 
 gulp.task('js', ['common-js'], function() {
 	return gulp.src([
-		// 'app/libs/jquery/dist/jquery.min.js',
+		'app/libs/jquery/dist/jquery.min.js',
 		// 'app/libs/bootstrap/dist/js/bootstrap.min.js',
 		'app/js/common.min.js', // always last
 		])
