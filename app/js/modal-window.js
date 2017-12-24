d91144b4b6b823be5d1ca89160e939596825a64e
vldcher <broobroo007@gmail.com>
2017-12-24 21:49:40 +0200

    get data about room from JSON file

--- app/js/modal-window.js
+++ app/js/modal-window.js
@@ -1,21 +1,28 @@
 // get the mPopup
-var mpopup = document.getElementById('mpopupBox');
+// var mpopup = document.getElementById('mpopupBox');
 
 // get the link that opens the mPopup
-var mpLink = document.getElementById("mpopupLink");
+// var mpLink = document.getElementsByClassName("mpopupLink")[0];
 
 // get the close action element
 var close = document.getElementsByClassName("close")[0];
 
 // open the mPopup once the link is clicked
-mpLink.onclick = function() {
-    mpopup.style.display = "block";
-}
+// mpLink.onclick = function() {
+//     mpopup.style.display = "block";
+// }
+
+$('.mpopupLink').bind('click', function(){
+
+	alert('hi');
+	// $('.mpopupBox').css("display", "block");
+    
+});
 
 // close the mPopup once close element is clicked
-close.onclick = function() {
-    mpopup.style.display = "none";
-}
+// close.onclick = function() {
+//     mpopup.style.display = "none";
+// }
 
 // close the mPopup when user clicks outside of the box
 window.onclick = function(event) {
--- app/js/reservation.js
+++ app/js/reservation.js
@@ -1,6 +1,59 @@
 $(function() {
 	$.getJSON('../data/rooms.json', function(data) {
-		console.log(data);
-	});
-	console.log('hel');
+	var output = '<div class="reservation__rooms">';
+		$.each(data, function(key, val) {
+			output += '<div class="available-room-item">';
+				output += '<div class="available-room-item__image">';
+					output += '<img src="../img/reservation-previews/'+ val.imageName +'.jpg" alt="'+ val.type +'" />';
+				output += '</div>';
+
+				output += '<div class="available-room-item__info">';
+					output += '<div class="available-room__header">';
+						output += '<p>'+ val.type +'</p>';
+					output += '</div>';
+
+					output += '<div class="available-room__guest-quantity">';
+						output += '<p>'+ 'Accomodation for :' +'</p>';
+						output += '<p class="room-guest-quantity">'+ val.roomCapacity + 'guest(s)' +'</p>';
+					output += '</div>';
+
+					output += '<div class="available-room__price">';
+						output += '<p class="price-field">'+ 'Price :' + val.price + '</p>';
+						output += '<p class="per-night">' + 'per night' +'</p>';
+					output += '</div>';
+
+				output += '</div>';
+
+				output += '<div class="available-room-item__arrow">';
+					output += '<a href="javascript:void(0);" class="mpopupLink">' +' <i class="fa fa-chevron-right">' + '</i>'+'</a>';
+					
+					output += '<div class="mpopup mpopupBox">';
+						output += '<div class="mpopup-content">';
+
+							output += '<div class="mpopup-head">';
+								output += '<span class="close">' + 'x' + '</span>';
+								output += '<h2>' + 'Reservation' + '</h2>';
+							output += '</div>';
+
+							output += '<div class="mpopup-main">';
+								output += '<p>' + 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam corporis quas repellendus unde minima, consectetur commodi consequatur quam, iure, ea veniam. Tempora cum dolorum quaerat, saepe magni eaque repudiandae aperiam!' + '</p>';
+							output += '</div>';
+
+							output += '<div class="mpopup-foot">';
+								output += '<p>' + 'Hotel Locanda' + '</p>';
+							output += '</div>';
+
+						output += '</div>';
+					output += '</div>';
+				output += '</div>';
+
+
+				
+
+			
+			output += '</div>';
+		});
+	output += '</div>';
+	$('#update').html(output);
+}); //get JSON
 });
